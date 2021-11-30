dda187726393ff4ca89cd70179e72338586faa03
Natanael Melo <79292912+natanaellmelo@users.noreply.github.com>
2021-11-30 12:37:45 -0300

    testes
    testando

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React, { Component }  from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { hydrate } from 'react-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Olá Mundo! Meu primeiro teste em react.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Olá Mundo!
+      </h1>
     </div>
   );
 }
